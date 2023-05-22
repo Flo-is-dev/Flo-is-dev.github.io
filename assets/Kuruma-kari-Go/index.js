7c8514898e7cf9a2e06289201d06546a9af33e84
Flo-le-dev <f.gironde.dev@gmail.com>
2023-05-23 00:08:47 +0200

    CRG v1.2

--- assets/Kuruma-kari-Go/index.js
+++ assets/Kuruma-kari-Go/index.js
@@ -1,3 +1,22 @@
+// **********************
+// **********************
+// Barre modal
+// ***************************
+// **********************
+
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach((trigger) =>
+  trigger.addEventListener("click", () => {
+    toggleModal();
+  })
+);
+
+const toggleModal = () => {
+  modalContainer.classList.toggle("active");
+};
+
 // ************************
 // Card de reservation/prix
 // *****************************
@@ -56,3 +75,147 @@ start_date.addEventListener("change", bookingCalc);
 end_date.addEventListener("change", bookingCalc);
 
 bookingCalc();
+
+// **********************
+// **********************
+// Formulaire d'inscription
+// ***************************
+// **********************
+
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll(
+  "input[type='text'], input[type='password']"
+);
+const progressBar = document.getElementById("progress-bar");
+let pseudo, email, password, confirmPass;
+
+// console.log(inputs);
+
+const errorDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+};
+
+const pseudoChecker = (value) => {
+  // console.log(value);
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 caractères");
+    pseudo = null;
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    errorDisplay(
+      "pseudo",
+      "Le pseudo ne doit pas contenir de caractères spéciaux"
+    );
+    pseudo = null;
+  } else {
+    errorDisplay("pseudo", "", true);
+    pseudo = value;
+  }
+};
+
+const emailChecker = (value) => {
+  // console.log(value);
+  if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+    errorDisplay("email", "Le mail n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
+};
+
+const passwordChecker = (value) => {
+  // console.log(value);
+  progressBar.classList = "";
+  if (
+    !value.match(
+      /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
+    )
+  ) {
+    // console.log("test");
+    errorDisplay(
+      "password",
+      "Minimum de 8 caractères, une majuscule, un chiffre et un caractère spécial"
+    );
+    progressBar.classList.add("progressRed");
+    password = null;
+  } else if (value.length < 12) {
+    progressBar.classList.add("progressBlue");
+    errorDisplay("password", "", true);
+    password = value;
+  } else {
+    progressBar.classList.add("progressGreen");
+    errorDisplay("password", "", true);
+    password = value;
+  }
+  if (confirmPass) confirmChecker(confirmPass);
+};
+
+const confirmChecker = (value) => {
+  // console.log(value);
+  if (value !== password) {
+    // console.log("error");
+    errorDisplay("confirm", "Les mots de passe ne correspondent pas");
+    confirmPass = false;
+  } else {
+    errorDisplay("confirm", "", true);
+    confirmPass = true;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    // console.log(e.target.id);
+    // console.log(e.target.value); ce qui est tapper à cahque fois
+    switch (e.target.id) {
+      case "pseudo":
+        pseudoChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+      case "confirm":
+        confirmChecker(e.target.value);
+        break;
+      default:
+        nul;
+    }
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (pseudo && email && password && confirmPass) {
+    const data = {
+      pseudo: pseudo,
+      email: email,
+      password: password,
+    };
+    console.log(data);
+
+    inputs.forEach((input) => (input.value = ""));
+    progressBar.classList = "";
+
+    pseudo = null;
+    email = null;
+    password = null;
+    confirmPass = null;
+    alert("Inscription validée (fictivement)");
+  } else {
+    alert(
+      "Veuillez remplir correctement les champs pour valider l'inscription (fictivement)"
+    );
+  }
+});
